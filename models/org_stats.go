d98fb7ef8f360abe60795d73f2972f90bf78cf5e
Aaron Gershman <aegershman@gmail.com>
2019-11-24 22:14:58 -0600

    separate out how org and space stats get generated

--- models/org_stats.go
+++ models/org_stats.go
@@ -3,30 +3,36 @@ package models
 // Stats -
 func (orgs Orgs) Stats(c chan OrgStats) {
 	for _, org := range orgs {
-
-		totalUniqueApps := org.AppsCount()
-		runningUniqueApps := org.RunningAppsCount()
-		stoppedUniqueApps := totalUniqueApps - runningUniqueApps
-
-		appInstancesCount := org.AppInstancesCount()
-		runningAppInstancesCount := org.RunningAppInstancesCount()
-		stoppedAppInstancesCount := appInstancesCount - runningAppInstancesCount
-
-		servicesCount := org.ServicesCount()
-
-		c <- OrgStats{
-			Name:                     org.Name,
-			MemoryQuota:              org.MemoryQuota,
-			MemoryUsage:              org.MemoryUsage,
-			Spaces:                   org.Spaces,
-			AppsCount:                totalUniqueApps,
-			RunningAppsCount:         runningUniqueApps,
-			StoppedAppsCount:         stoppedUniqueApps,
-			AppInstancesCount:        appInstancesCount,
-			RunningAppInstancesCount: runningAppInstancesCount,
-			StoppedAppInstancesCount: stoppedAppInstancesCount,
-			ServicesCount:            servicesCount,
-		}
+		orgStats := NewOrgStats(org)
+		c <- orgStats
 	}
 	close(c)
+}
+
+// NewOrgStats converts an Org into something decorated with more information
+// that can be used for presenting business logic and such
+func NewOrgStats(org Org) OrgStats {
+	totalUniqueApps := org.AppsCount()
+	runningUniqueApps := org.RunningAppsCount()
+	stoppedUniqueApps := totalUniqueApps - runningUniqueApps
+
+	appInstancesCount := org.AppInstancesCount()
+	runningAppInstancesCount := org.RunningAppInstancesCount()
+	stoppedAppInstancesCount := appInstancesCount - runningAppInstancesCount
+
+	servicesCount := org.ServicesCount()
+
+	return OrgStats{
+		Name:                     org.Name,
+		MemoryQuota:              org.MemoryQuota,
+		MemoryUsage:              org.MemoryUsage,
+		Spaces:                   org.Spaces,
+		AppsCount:                totalUniqueApps,
+		RunningAppsCount:         runningUniqueApps,
+		StoppedAppsCount:         stoppedUniqueApps,
+		AppInstancesCount:        appInstancesCount,
+		RunningAppInstancesCount: runningAppInstancesCount,
+		StoppedAppInstancesCount: stoppedAppInstancesCount,
+		ServicesCount:            servicesCount,
+	}
 }
--- models/space_stats.go
+++ models/space_stats.go
@@ -3,42 +3,48 @@ package models
 // Stats -
 func (spaces Spaces) Stats(c chan SpaceStats, skipSIcount bool) {
 	for _, space := range spaces {
-
-		totalUniqueApps := space.AppsCount()
-		runningUniqueApps := space.RunningAppsCount()
-		stoppedUniqueApps := totalUniqueApps - runningUniqueApps
-
-		// What _used_ to be reported as just "services"
-		servicesSuiteForPivotalPlatformCount := space.ServicesSuiteForPivotalPlatformCount()
-
-		appInstancesCount := space.AppInstancesCount()
-		runningAppInstancesCount := space.RunningAppInstancesCount()
-		stoppedAppInstancesCount := appInstancesCount - runningAppInstancesCount
-
-		billableAppInstancesCount := space.AppInstancesCount()
-		billableAppInstancesCount += space.SpringCloudServicesCount()
-
-		consumedMemory := space.ConsumedMemory()
-		servicesCount := space.ServicesCount()
-		billableServicesCount := servicesCount - space.SpringCloudServicesCount()
-		if skipSIcount {
-			servicesCount = 0
-		}
-
-		c <- SpaceStats{
-			Name:                                 space.Name,
-			AppsCount:                            totalUniqueApps,
-			RunningAppsCount:                     runningUniqueApps,
-			StoppedAppsCount:                     stoppedUniqueApps,
-			AppInstancesCount:                    appInstancesCount,
-			RunningAppInstancesCount:             runningAppInstancesCount,
-			StoppedAppInstancesCount:             stoppedAppInstancesCount,
-			ServicesCount:                        servicesCount,
-			ConsumedMemory:                       consumedMemory,
-			ServicesSuiteForPivotalPlatformCount: servicesSuiteForPivotalPlatformCount,
-			BillableAppInstancesCount:            billableAppInstancesCount,
-			BillableServicesCount:                billableServicesCount,
-		}
+		spaceStats := NewSpaceStats(space, skipSIcount)
+		c <- spaceStats
 	}
 	close(c)
+}
+
+// NewSpaceStats converts a Space into something decorated with more information
+// that can be used for presenting business logic and such
+func NewSpaceStats(space Space, skipSIcount bool) SpaceStats {
+	totalUniqueApps := space.AppsCount()
+	runningUniqueApps := space.RunningAppsCount()
+	stoppedUniqueApps := totalUniqueApps - runningUniqueApps
+
+	servicesSuiteForPivotalPlatformCount := space.ServicesSuiteForPivotalPlatformCount()
+
+	appInstancesCount := space.AppInstancesCount()
+	runningAppInstancesCount := space.RunningAppInstancesCount()
+	stoppedAppInstancesCount := appInstancesCount - runningAppInstancesCount
+
+	billableAppInstancesCount := space.AppInstancesCount()
+	billableAppInstancesCount += space.SpringCloudServicesCount()
+
+	consumedMemory := space.ConsumedMemory()
+	servicesCount := space.ServicesCount()
+	billableServicesCount := servicesCount - space.SpringCloudServicesCount()
+	if skipSIcount {
+		servicesCount = 0
+	}
+
+	return SpaceStats{
+		Name:                                 space.Name,
+		AppsCount:                            totalUniqueApps,
+		RunningAppsCount:                     runningUniqueApps,
+		StoppedAppsCount:                     stoppedUniqueApps,
+		AppInstancesCount:                    appInstancesCount,
+		RunningAppInstancesCount:             runningAppInstancesCount,
+		StoppedAppInstancesCount:             stoppedAppInstancesCount,
+		ServicesCount:                        servicesCount,
+		ConsumedMemory:                       consumedMemory,
+		ServicesSuiteForPivotalPlatformCount: servicesSuiteForPivotalPlatformCount,
+		BillableAppInstancesCount:            billableAppInstancesCount,
+		BillableServicesCount:                billableServicesCount,
+	}
+
 }
