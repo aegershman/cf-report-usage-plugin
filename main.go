8f3303949de46b9f694321779d1a7a229f7d94bd
Aaron Gershman <aegershman@gmail.com>
2019-11-25 11:26:42 -0600

    going to rename to usagereport

--- main.go
+++ main.go
@@ -36,8 +36,8 @@ func (cmd *UsageReportCmd) GetMetadata() plugin.PluginMetadata {
 		Name: "cf-trueup-plugin",
 		Version: plugin.VersionType{
 			Major: 2,
-			Minor: 7,
-			Build: 6,
+			Minor: 8,
+			Build: 0,
 		},
 		Commands: []plugin.Command{
 			{
