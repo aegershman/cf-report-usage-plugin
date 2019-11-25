28950306f3083e41be1cfd3a448a388fa8dd11c0
Aaron Gershman <aegershman@gmail.com>
2019-11-24 23:41:37 -0600

    and voila, table should have it presented as well

--- presenters/table_presenter.go
+++ presenters/table_presenter.go
@@ -4,35 +4,26 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/aegershman/cf-trueup-plugin/models"
 	"github.com/olekukonko/tablewriter"
 )
 
+// TODO just testing, just goofing off, I know this is wrong, etc...
 func (p *Presenter) asTable() {
 
-	chOrgStats := make(chan models.OrgStats, len(p.Report.Orgs))
+	for _, orgStats := range p.Report.OrgStats {
 
-	go p.Report.Orgs.Stats(chOrgStats)
-	for orgStats := range chOrgStats {
-
-		chSpaceStats := make(chan models.SpaceStats, len(orgStats.Spaces))
-
-		// TODO dynamic filtering?
-		go orgStats.Spaces.Stats(chSpaceStats, orgStats.Name == "p-spring-cloud-services")
-
-		// TODO just testing, just goofing off, I know this is wrong, etc...
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetAlignment(tablewriter.ALIGN_LEFT)
 		table.SetHeader([]string{"Space", "MB", "cAIs", "bAIs", "cSIs", "bSIs"})
 
-		for spaceState := range chSpaceStats {
+		for _, spaceStat := range orgStats.SpaceStats {
 			table.Append([]string{
-				spaceState.Name,
-				strconv.Itoa(spaceState.ConsumedMemory),
-				strconv.Itoa(spaceState.AppInstancesCount),
-				strconv.Itoa(spaceState.BillableAppInstancesCount),
-				strconv.Itoa(spaceState.ServicesCount),
-				strconv.Itoa(spaceState.BillableServicesCount),
+				spaceStat.Name,
+				strconv.Itoa(spaceStat.ConsumedMemory),
+				strconv.Itoa(spaceStat.AppInstancesCount),
+				strconv.Itoa(spaceStat.BillableAppInstancesCount),
+				strconv.Itoa(spaceStat.ServicesCount),
+				strconv.Itoa(spaceStat.BillableServicesCount),
 			})
 		}
 
