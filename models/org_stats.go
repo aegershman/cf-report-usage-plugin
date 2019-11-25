2911f3e7145fe5613e91f840acbba3d56cbf04e0
Aaron Gershman <aegershman@users.noreply.github.com>
2019-11-24 22:18:56 -0600

    break out org and space stats into their own definitions (#41)
    * break out org and space stats into their own files
    
    * separate out how org and space stats get generated

---
+++ models/org_stats.go
@@ -0,0 +1,38 @@
+package models
+
+// Stats -
+func (orgs Orgs) Stats(c chan OrgStats) {
+	for _, org := range orgs {
+		orgStats := NewOrgStats(org)
+		c <- orgStats
+	}
+	close(c)
+}
+
+// NewOrgStats converts an Org into something decorated with more information
+// that can be used for presenting business logic and such
+func NewOrgStats(org Org) OrgStats {
+	totalUniqueApps := org.AppsCount()
+	runningUniqueApps := org.RunningAppsCount()
+	stoppedUniqueApps := totalUniqueApps - runningUniqueApps
+
+	appInstancesCount := org.AppInstancesCount()
+	runningAppInstancesCount := org.RunningAppInstancesCount()
+	stoppedAppInstancesCount := appInstancesCount - runningAppInstancesCount
+
+	servicesCount := org.ServicesCount()
+
+	return OrgStats{
+		Name:                     org.Name,
+		MemoryQuota:              org.MemoryQuota,
+		MemoryUsage:              org.MemoryUsage,
+		Spaces:                   org.Spaces,
+		AppsCount:                totalUniqueApps,
+		RunningAppsCount:         runningUniqueApps,
+		StoppedAppsCount:         stoppedUniqueApps,
+		AppInstancesCount:        appInstancesCount,
+		RunningAppInstancesCount: runningAppInstancesCount,
+		StoppedAppInstancesCount: stoppedAppInstancesCount,
+		ServicesCount:            servicesCount,
+	}
+}
