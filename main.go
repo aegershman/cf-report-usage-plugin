d4f2742bea9cdf27fafacc39d5dfd31a78fef7f3
Aaron Gershman <aegershman@users.noreply.github.com>
2019-11-24 21:56:50 -0600

    presenters to use lowercase naming format (#40)
    * rename presenters to use lower case format

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func (cmd *UsageReportCmd) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 2,
 			Minor: 7,
-			Build: 3,
+			Build: 4,
 		},
 		Commands: []plugin.Command{
 			{
