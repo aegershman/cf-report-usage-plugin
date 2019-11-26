44d5710e082d37321cf5d1d9e4749582191d4fae
Aaron Gershman <aegershman@gmail.com>
2019-11-26 15:17:28 -0600

    bump version

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func (cmd *UsageReportCmd) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 2,
 			Minor: 8,
-			Build: 6,
+			Build: 7,
 		},
 		Commands: []plugin.Command{
 			{
