36c06be0c68e182d23ee0c3b3cd19651dfb6f8b7
Aaron Gershman <aegershman@gmail.com>
2019-11-26 11:54:15 -0600

    shorten up string presenter

--- presenters/string_presenter.go
+++ presenters/string_presenter.go
@@ -11,12 +11,9 @@ func (p *Presenter) asString() {
 	const (
 		orgOverviewMsg               = "Org %s is consuming %d MB of %d MB.\n"
 		spaceOverviewMsg             = "\tSpace %s is consuming %d MB memory (%d%%) of org quota.\n"
-		spaceAppInstancesMsg         = "\t\t%d app instances: %d running %d stopped\n"
-		spaceBillableAppInstancesMsg = "\t\t%d billable app instances (includes AIs and billable SIs, like SCS)\n"
-		spaceUniqueAppGuidsMsg       = "\t\t%d unique app_guids: %d running %d stopped\n"
-		spaceServiceMsg              = "\t\t%d service instances total\n"
-		spaceServiceSuiteMsg         = "\t\t%d service instances of type Service Suite (mysql, redis, rmq)\n"
-		spaceSCSMsg                  = "\t\t%d service instances of type SCS (config-server, eureka, etc.)\n"
+		spaceBillableAppInstancesMsg = "\t\t%d billable AIs (includes AIs and billable SIs, like SCS)\n"
+		spaceAppInstancesMsg         = "\t\t%d AIs: %d running %d stopped\n"
+		spaceSCSMsg                  = "\t\t%d SIs of type SCS (config-server, eureka, etc.)\n"
 		reportSummaryMsg             = "Across %d org(s), you have %d billable AIs, %d are canonical AIs (%d running, %d stopped), %d are SCS instances\n"
 	)
 
@@ -27,12 +24,9 @@ func (p *Presenter) asString() {
 				spaceMemoryConsumedPercentage := (100 * spaceStat.ConsumedMemory / orgStat.MemoryQuota)
 				response.WriteString(fmt.Sprintf(spaceOverviewMsg, spaceStat.Name, spaceStat.ConsumedMemory, spaceMemoryConsumedPercentage))
 			}
-			response.WriteString(fmt.Sprintf(spaceAppInstancesMsg, spaceStat.AppInstancesCount, spaceStat.RunningAppInstancesCount, spaceStat.StoppedAppInstancesCount))
 			response.WriteString(fmt.Sprintf(spaceBillableAppInstancesMsg, spaceStat.BillableAppInstancesCount()))
-			response.WriteString(fmt.Sprintf(spaceUniqueAppGuidsMsg, spaceStat.AppsCount, spaceStat.RunningAppsCount, spaceStat.StoppedAppsCount))
-			response.WriteString(fmt.Sprintf(spaceServiceMsg, spaceStat.ServicesCount))
+			response.WriteString(fmt.Sprintf(spaceAppInstancesMsg, spaceStat.AppInstancesCount, spaceStat.RunningAppInstancesCount, spaceStat.StoppedAppInstancesCount))
 			response.WriteString(fmt.Sprintf(spaceSCSMsg, spaceStat.SpringCloudServicesCount()))
-			response.WriteString(fmt.Sprintf(spaceServiceSuiteMsg, spaceStat.ServicesSuiteForPivotalPlatformCount()))
 		}
 	}
 
