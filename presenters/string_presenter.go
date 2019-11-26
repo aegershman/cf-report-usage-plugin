833b7e346be90341488226ea4f828002edfb246f
Aaron Gershman <aegershman@gmail.com>
2019-11-26 11:59:08 -0600

    personal preferences on string presenter, not married to it if other people like it differently

--- presenters/string_presenter.go
+++ presenters/string_presenter.go
@@ -9,12 +9,12 @@ func (p *Presenter) asString() {
 	var response bytes.Buffer
 
 	const (
-		orgOverviewMsg               = "Org %s is consuming %d MB of %d MB.\n"
-		spaceOverviewMsg             = "\tSpace %s is consuming %d MB memory (%d%%) of org quota.\n"
-		spaceBillableAppInstancesMsg = "\t\t%d billable AIs (includes AIs and billable SIs, like SCS)\n"
-		spaceAppInstancesMsg         = "\t\t%d AIs: %d running %d stopped\n"
-		spaceSCSMsg                  = "\t\t%d SIs of type SCS (config-server, eureka, etc.)\n"
-		reportSummaryMsg             = "Across %d org(s), you have %d billable AIs, %d are canonical AIs (%d running, %d stopped), %d are SCS instances\n"
+		orgOverviewMsg               = "org %s is consuming %d MB of %d MB\n"
+		spaceOverviewMsg             = "\tspace %s is consuming %d MB memory (%d%%) of org quota\n"
+		spaceBillableAppInstancesMsg = "\t\tAIs billable: %d\n"
+		spaceAppInstancesMsg         = "\t\tAIs canonical: %d (%d running, %d stopped)\n"
+		spaceSCSMsg                  = "\t\tSCS instances: %d\n"
+		reportSummaryMsg             = "across %d org(s), you have %d billable AIs, %d are canonical AIs (%d running, %d stopped), %d are SCS instances\n"
 	)
 
 	for _, orgStat := range p.Report.OrgStats {
