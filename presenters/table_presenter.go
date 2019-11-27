e47e3ced45a1200710eab30cfcebfb46eb71143a
Aaron Gershman <aegershman@gmail.com>
2019-11-27 14:35:25 -0600

    swap out reporter for report

--- presenters/table_presenter.go
+++ presenters/table_presenter.go
@@ -19,7 +19,7 @@ func (p *Presenter) asTable() {
 		"SCS",
 	})
 
-	for _, orgReport := range p.SummaryReporter.OrgReports() {
+	for _, orgReport := range p.SummaryReport.OrgReports() {
 		for _, spaceReport := range orgReport.SpaceReports() {
 			table.Append([]string{
 				orgReport.Name(),
@@ -35,10 +35,10 @@ func (p *Presenter) asTable() {
 	table.SetFooter([]string{
 		"-",
 		"Total",
-		strconv.Itoa(p.SummaryReporter.BillableAppInstancesCount()),
-		strconv.Itoa(p.SummaryReporter.AppInstancesCount()),
-		strconv.Itoa(p.SummaryReporter.StoppedAppInstancesCount()),
-		strconv.Itoa(p.SummaryReporter.SpringCloudServicesCount()),
+		strconv.Itoa(p.SummaryReport.BillableAppInstancesCount()),
+		strconv.Itoa(p.SummaryReport.AppInstancesCount()),
+		strconv.Itoa(p.SummaryReport.StoppedAppInstancesCount()),
+		strconv.Itoa(p.SummaryReport.SpringCloudServicesCount()),
 	})
 
 	table.Render()
