2888077b2ac4bd7c1102b33d448a300075ff8c80
Aaron Gershman <aegershman@gmail.com>
2019-11-27 12:37:16 -0600

    whelp. Alright, so don't use interfaces, perhaps? Go figure something else out for reporting

--- presenters/json_presenter.go
+++ presenters/json_presenter.go
@@ -1,5 +1,9 @@
 package presenters
 
-func (p *Presenter) asJSON() {
+import (
+	log "github.com/sirupsen/logrus"
+)
 
+func (p *Presenter) asJSON() {
+	log.Fatalln("uhhh... TODO. You probably shouldn't have used interfaces for the reporters, you should 've used structs. But it made sense at the time.")
 }
