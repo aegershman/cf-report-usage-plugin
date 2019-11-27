3d7b3a7acd75ef98969abd0ba15066943032f37e
Aaron Gershman <aegershman@gmail.com>
2019-11-27 14:21:44 -0600

    lets do this again except with structs this time

--- models/reporter.go
+++ models/reporter.go
@@ -1,5 +1,23 @@
 package models
 
+// Report -
+type Report struct {
+	AppInstancesCount                    int
+	AppsCount                            int
+	BillableAppInstancesCount            int
+	BillableServicesCount                int
+	MemoryQuota                          int
+	MemoryUsage                          int
+	Name                                 string
+	RunningAppInstancesCount             int
+	RunningAppsCount                     int
+	ServicesCount                        int
+	ServicesSuiteForPivotalPlatformCount int
+	SpringCloudServicesCount             int
+	StoppedAppInstancesCount             int
+	StoppedAppsCount                     int
+}
+
 // Reporter -
 type Reporter interface {
 	AppInstancesCount() int
