8da4b4043a990f6be272df5791d5b0fe2765ebdb
Aaron Gershman <aegershman@gmail.com>
2019-11-26 20:25:39 -0600

    my bad, add back space overview message, just without the percentage used

--- presenters/string_presenter.go
+++ presenters/string_presenter.go
@@ -10,6 +10,7 @@ func (p *Presenter) asString() {
 
 	const (
 		orgOverviewMsg               = "org %s is consuming %d MB of %d MB\n"
+		spaceOverviewMsg             = "\tspace %s is consuming %d MB memory of org quota\n"
 		spaceBillableAppInstancesMsg = "\t\tAIs billable: %d\n"
 		spaceAppInstancesMsg         = "\t\tAIs canonical: %d (%d running, %d stopped)\n"
 		spaceSCSMsg                  = "\t\tSCS instances: %d\n"
@@ -19,6 +20,7 @@ func (p *Presenter) asString() {
 	for _, orgReport := range p.Report.SummaryReport.OrgReports() {
 		response.WriteString(fmt.Sprintf(orgOverviewMsg, orgReport.Name(), orgReport.MemoryUsage(), orgReport.MemoryQuota()))
 		for _, spaceReport := range orgReport.SpaceReports() {
+			response.WriteString(fmt.Sprintf(spaceOverviewMsg, spaceReport.Name(), spaceReport.MemoryUsage()))
 			response.WriteString(fmt.Sprintf(spaceBillableAppInstancesMsg, spaceReport.BillableAppInstancesCount()))
 			response.WriteString(fmt.Sprintf(spaceAppInstancesMsg, spaceReport.AppInstancesCount(), spaceReport.RunningAppInstancesCount(), spaceReport.StoppedAppInstancesCount()))
 			response.WriteString(fmt.Sprintf(spaceSCSMsg, spaceReport.SpringCloudServicesCount()))
