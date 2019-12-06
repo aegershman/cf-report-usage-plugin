d5ec0d0b438153e9517f5a224ef125b86c287880
Aaron Gershman <aegershman@gmail.com>
2019-12-06 14:46:29 -0600

    plop cfcurl over to client in substitution of proper do method

--- v2client/client.go
+++ v2client/client.go
@@ -1,12 +1,20 @@
 package v2client
 
 import (
+	"encoding/json"
+	"errors"
+	"strings"
+
 	"github.com/cloudfoundry/cli/plugin"
 )
 
 // Client -
 type Client struct {
 	cli plugin.CliConnection
+}
+
+type service struct {
+	client *Client
 }
 
 // NewClient -
@@ -16,6 +24,28 @@ func NewClient(cli plugin.CliConnection) *Client {
 	}
 }
 
-type service struct {
-	client *Client
+// Curl -
+func (c *Client) Curl(path string) (map[string]interface{}, error) {
+	output, err := c.cli.CliCommandWithoutTerminalOutput("curl", path)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseOutput(output)
+}
+
+func parseOutput(output []string) (map[string]interface{}, error) {
+	if nil == output || 0 == len(output) {
+		return nil, errors.New("CF API returned no output")
+	}
+
+	data := strings.Join(output, "\n")
+
+	if 0 == len(data) || "" == data {
+		return nil, errors.New("Failed to join output")
+	}
+
+	var f interface{}
+	err := json.Unmarshal([]byte(data), &f)
+	return f.(map[string]interface{}), err
 }
